9416607bfac5022b395baead0b67951de7abca82
edvicaty <edvicaty@hotmail.com>
2021-03-04 12:41:05 -0600

    feat: add hover effect to CardBlock

--- src/components/Wrapper/index.js
+++ src/components/Wrapper/index.js
@@ -16,6 +16,9 @@ const Wrapper = ({ children, ...props }) => {
         boxShadow: "0px 0px 12px -2px rgba(0, 0, 0, 0.15)",
         width: "40vw",
         padding: "5px 0",
+        ":hover": {
+          boxShadow: "0px 0px 12px -2px rgba(0, 0, 0, 0.5)",
+        },
       }}
       {...props}
     >
