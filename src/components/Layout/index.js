04133af8e0c7365a852ab510fac51bbb9563139b
edvicaty <edvicaty@hotmail.com>
2021-03-04 09:49:46 -0600

    feat: add Lato font and web font loader plugin

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,7 +2,21 @@ import React from "react"
 import { Box } from "theme-ui"
 
 const Layout = ({ children, ...props }) => {
-  return <Box {...props}>{children}</Box>
+  return (
+    <Box
+      __css={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        textAlign: "center",
+        fontFamily: "Lato",
+        fontSize: "18px",
+      }}
+      {...props}
+    >
+      {children}
+    </Box>
+  )
 }
 
 export default Layout
