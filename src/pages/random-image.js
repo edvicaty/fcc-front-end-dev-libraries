f2c9a6796044103cd825844ac56b0d16b734c531
edvicaty <edvicaty@hotmail.com>
2021-03-08 13:57:19 -0600

    feat: testing various image formats

--- src/pages/random-image.js
+++ src/pages/random-image.js
@@ -2,64 +2,30 @@ import React, { useState } from "react"
 import fetch from "cross-fetch"
 import { StaticImage } from "gatsby-plugin-image"
 import { LayoutBlock, SectionBlock } from "../blocks"
-import { Button, Text, Link } from "../components"
-import {
-  navLinks,
-  imageHelpers,
-  randomQuotesHelpers,
-} from "../helpers/constants"
-import {
-  generateRandomArray,
-  getRandomItem,
-} from "../helpers/functions/generalFunctions"
-//TODO: static image html tag
-//TODO: static image gatsby-image
-//TODO: static image gatsby-plugin-image
-
-//TODO: fetched image html tag
-//TODO: fetched image gatsby-plugin-image
+import { Text, Wrapper } from "../components"
+import { navLinks, imageHelpers } from "../helpers/constants"
 
 const RandomImagePage = () => {
-  const [apiQuote, setApiQuote] = useState({
-    text: "Click 'New quote' to fetch from the API",
-    author: "Edgar Villavicencio",
-  })
-
   return (
     <LayoutBlock navLinks={navLinks}>
       <SectionBlock title={{ heading: "h1", text: "Image comparison" }}>
         <Text>Compare html image vs Gatsby Image</Text>
       </SectionBlock>
-
-      <SectionBlock
-        direction="column"
-        title={{
-          heading: "h2",
-          text: "html image from URL",
-        }}
-      >
-        <img src={imageHelpers.highResImage} alt="fetched html image" />
-      </SectionBlock>
-
-      {/* <SectionBlock
-        direction="column"
-        title={{
-          heading: "h2",
-          text: "new Gatsby image from URL",
-        }}
-      >
-        <StaticImage src={imageHelpers.highResImage} alt="fetched gatsby" />
-      </SectionBlock> */}
-
-      <SectionBlock
-        direction="column"
-        title={{
-          heading: "h2",
-          text: "new Gatsby image from 'images folder",
-        }}
-      >
-        <StaticImage src="../images/highRes.jpg" alt="static gatsby" />
-      </SectionBlock>
+      {imageHelpers &&
+        imageHelpers.map(image => {
+          return (
+            <SectionBlock border title={{ heading: "h2", text: image.name }}>
+              <Wrapper>
+                <Text as="h3">Gatsby</Text>
+                <StaticImage src={image.url} alt={image.name} />
+              </Wrapper>
+              <Wrapper>
+                <Text as="h3">HTML</Text>
+                <img src={image.url} alt={image.name} />
+              </Wrapper>
+            </SectionBlock>
+          )
+        })}
     </LayoutBlock>
   )
 }
