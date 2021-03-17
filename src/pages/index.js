eb176b2c74765676013465da1d815e52d702e058
edvicaty <edvicaty@hotmail.com>
2021-03-17 17:08:10 -0600

    feat: add initial redux code for dark mode

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,24 @@ import * as React from "react"
 import { LayoutBlock, SectionBlock } from "../blocks"
 import { Text } from "../components"
 import { navLinks } from "../helpers/constants"
+import { connect } from "react-redux"
 
-const IndexPage = () => (
+import { toggleDarkMode } from "../state/app"
+
+const IndexPage = ({ isDarkMode, dispatch }) => (
   <LayoutBlock navLinks={navLinks}>
     <SectionBlock
       title={{ heading: "h1", text: "FCC front-end libraries projects" }}
     >
       <Text>https://www.freecodecamp.org/learn/front-end-libraries/</Text>
     </SectionBlock>
+    <button onClick={() => { dispatch(toggleDarkMode(!isDarkMode)) }} >Dark Mode {isDarkMode ? "on" : "off"}</button>
   </LayoutBlock>
 )
 
-export default IndexPage
+
+export default connect(state=>({
+  isDarkMode: state.app.isDarkMode
+}), null)(IndexPage)
+
+// export default IndexPage
