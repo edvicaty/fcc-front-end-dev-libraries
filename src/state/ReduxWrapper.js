6bd8b9348ef2b32bf69098d33bd2b355e98a8a44
edvicaty <edvicaty@hotmail.com>
2021-03-18 10:54:49 -0600

    feat: add default state on store and refactor createStore

--- src/state/ReduxWrapper.js
+++ src/state/ReduxWrapper.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore as reduxCreateStore } from 'redux';
+// import { createStore as reduxCreateStore } from 'redux';
+import { createStore } from 'redux';
 import rootReducer from '.';
 
-const createStore = () => reduxCreateStore(rootReducer);
+// const createStore = () => reduxCreateStore(rootReducer);
+
+
+// export default ({ element }) => (
+//   <Provider store={createStore()}>{element}</Provider>
+// );
+
+const defaultState = {
+  isDarkMode: false
+}
+
+const store = createStore(rootReducer, defaultState)
 
 export default ({ element }) => (
-  <Provider store={createStore()}>{element}</Provider>
+  <Provider store={store}>{element}</Provider>
 );
